249edc94d08bb97f5c5fd30f9e2a31daf3366839
Pixeldeck <philipmir@hotmail.com>
2022-10-19 11:36:30 +0200

    Player and intro

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,8 @@ public class Main {
     public static void main(String[] args) {
         GameBord g1 = new GameBord();
         Player player = new Player();
-
         g1.print();
+
         player.startText();
-
-
-
-        
-
-
-
-
-
-
     }
 }
