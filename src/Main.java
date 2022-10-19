f725ae53fc3ff9c3aa3de3141fb3f8aec433a79a
Pixeldeck <philipmir@hotmail.com>
2022-10-19 11:23:45 +0200

    Created a player and intro

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
 public class Main {
     public static void main(String[] args) {
         GameBord g1 = new GameBord();
+        Player player = new Player();
+
         g1.print();
+        player.startText();
+
+
+
+        
+
+
+
+
+
+
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class Player {
+Scanner scan = new Scanner(System.in);
+    private static String name;
+
+    public Player() {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void startText() {
+        System.out.println("Welcome to MineSweeper!");
+        System.out.println("What is your name?");
+        Player.name = scan.nextLine();
+        System.out.println("Welcome " + name + ", type your first move, row and column:");
+
+    }
 }
