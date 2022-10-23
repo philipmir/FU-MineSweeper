a3cd783131ea15bd3ab47c50330dcfbfe30866eb
Pixeldeck <philipmir@hotmail.com>
2022-10-23 20:20:53 +0200

    Ask player how many mines and set it

--- src/Game.java
+++ src/Game.java
@@ -3,13 +3,18 @@ import java.util.Scanner;
 public class Game {
     static Scanner scan = new Scanner(System.in);
     static Player player = new Player();
+    static GameBord gamebord = new GameBord();
+
+    static int mineCount;
 
 
     public static void startText() {
         System.out.println("Welcome to MineSweeper!");
         System.out.println("What is your name?");
         player.setName(scan.nextLine());
-        System.out.println("Welcome " + player.getName() + ", type your first move, row and column:");
+        System.out.println("How many mines do you want?");
+        mineCount = scan.nextInt();
+        System.out.println("Welcome " + player.getName() + ". You are playing with " + mineCount + " mines, type your first move, row and column:");
 
     }
 
