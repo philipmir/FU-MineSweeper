7792a887cb09a9d0be9030d8ea2b41e3a689da89
Pixeldeck <philipmir@hotmail.com>
2022-10-23 20:23:54 +0200

    Forgot text

--- src/Game.java
+++ src/Game.java
@@ -12,7 +12,7 @@ public class Game {
         System.out.println("Welcome to MineSweeper!");
         System.out.println("What is your name?");
         player.setName(scan.nextLine());
-        System.out.println("How many mines do you want?");
+        System.out.println("How many mines do you want? 1-19");
         mineCount = scan.nextInt();
         System.out.println("Welcome " + player.getName() + ". You are playing with " + mineCount + " mines, type your first move, row and column:");
 
